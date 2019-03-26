e1c034dec2803bba27033a8ae9b0fdfe7e28cdfe
mahmoodisaeed <mahmoodi.saeed@gmail.com>
2019-03-27 02:19:23 +0430

    activity scenarios

--- app/src/main/java/com/example/myapp5/MainActivity.java
+++ app/src/main/java/com/example/myapp5/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapp5;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("------","CREATE");
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("------","START");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("------","RESTART");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+            Log.d("------","RESUME");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("------","PAUSE");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("------","STOP");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("------","DESTROY");
     }
 }
